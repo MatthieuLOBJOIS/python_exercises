14149ee9c0e1d1784c0e9a072b1434b0708c019e
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2023-04-01 18:48:34 +0200

    add project 1 : la calculatrice

---
+++ project_1_la_calculatrice/la_calculatrice.py
@@ -0,0 +1,7 @@
+# Demander à l'utilisateur d'entrer un premier nombre
+# Demander à l'utilisateur d'entrer un deuxième nombre
+# Afficher à l'écran le résultat de l'addition (exemple : 'Le résultat de l'addition de 5 + 10 est égal à 15')
+
+first_number = int(input("Entrée un premier nombre : "))
+second_number = int(input("Entrée un deuxième nombre : "))
+result = f"Le résultat de l'addition de {first_number} + {second_number} est égal à {first_number + second_number}"
