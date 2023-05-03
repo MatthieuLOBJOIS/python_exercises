e77a3a1dfc328d779fe2a16bcf590cb286bb5170
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2023-05-03 23:38:03 +0200

    project 10 : add log info management

--- project_10_le_gestionnaire_utilisateurs/user.py
+++ project_10_le_gestionnaire_utilisateurs/user.py
@@ -1,5 +1,10 @@
 """Module to generate a random users"""
 import faker
+import logging
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+logging.basicConfig(filename=BASE_DIR / "user.log", level=logging.DEBUG)
 
 def get_user() -> str:
     """Generate a single user
@@ -10,8 +15,9 @@ def get_user() -> str:
     fake = faker.Faker()
     first_name = fake.first_name()
     last_name = fake.last_name()
+    logging.info("Genarating user.")
     return f"{first_name} {last_name}"
-    
+
 def get_users(number: int) -> list[str]:
     """Generate a list of users
 
@@ -21,6 +27,7 @@ def get_users(number: int) -> list[str]:
     Returns:
         list[str]: users
     """
+    logging.info(f"Generating a list of {number} users.")
     return [get_user() for _ in range(number)]
 
 if __name__ == "__main__":
