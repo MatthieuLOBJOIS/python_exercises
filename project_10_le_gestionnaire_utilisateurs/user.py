1734c7faa999f31172cdf579269a538765064fb7
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2023-05-03 22:58:50 +0200

    project 10 : add docstring (module user)

--- project_10_le_gestionnaire_utilisateurs/user.py
+++ project_10_le_gestionnaire_utilisateurs/user.py
@@ -1,13 +1,27 @@
+"""Module to generate a random users"""
 import faker
 
 def get_user() -> str:
+    """Generate a single user
+
+    Returns:
+        str: user
+    """
     fake = faker.Faker()
     first_name = fake.first_name()
     last_name = fake.last_name()
     return f"{first_name} {last_name}"
     
-def get_users(number: int) -> list:
-    print([get_user() for _ in range(number)])
+def get_users(number: int) -> list[str]:
+    """Generate a list of users
+
+    Args:
+        number (int): number of users to generate
+
+    Returns:
+        list[str]: users
+    """
+    return [get_user() for _ in range(number)]
 
 if __name__ == "__main__":
     get_users(6)
