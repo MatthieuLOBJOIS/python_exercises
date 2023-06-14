2985deb37bd1c9c6ba20d988d87b3fa310f01fab
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2023-06-14 18:01:34 +0200

    add method ajouter and enlever

--- project_3_2_la_liste_de_course_poo/lib.py
+++ project_3_2_la_liste_de_course_poo/lib.py
@@ -1,6 +1,29 @@
+import logging
+
+LOGGER = logging.getLogger()
+
 class Liste(list):
     def __init__(self, nom):
         self.nom = nom
+    
+    def ajouter(self, element):
+        if not isinstance(element, str):
+           raise ValueError("Vous ne pouvez ajouter que des chaîne de caractères !") 
+            
+        if element in self:   
+            LOGGER.error(f"{element} est déjà dans la liste.")
+            return False
         
-if __name__ == "main":
-    pass
+        self.append(element)
+        return True
+    
+    def enlever(self, element):
+        if element in self:
+            self.remove(element)
+            return True
+        return False
+              
+if __name__ == "__main__":
+    liste = Liste("courses")
+    liste.ajouter("Pomme")
+    print(liste)
