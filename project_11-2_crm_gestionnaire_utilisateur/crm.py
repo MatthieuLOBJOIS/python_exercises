96deaac73ddcc68c007d633caa714c57ebb58883
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2024-05-01 17:58:18 +0200

    project 11-2: add a check names method

--- project_11-2_crm_gestionnaire_utilisateur/crm.py
+++ project_11-2_crm_gestionnaire_utilisateur/crm.py
@@ -1,4 +1,5 @@
 import re
+import string
 
 class User:
     def __init__(self, first_name: str, last_name: str, phone_number: str="", address: str=""):
@@ -22,11 +23,20 @@ class User:
         if len(phone_number) < 10 or not phone_number.isdigit():
             raise ValueError(f"Numéro de téléphone {self.phone_number} invalide.")
  
+    def _check_names(self):
+        if not(self.first_name and self.last_name):
+            raise ValueError("Le prénom et le nom de famille ne peuvent pas être vides.")
+
+        special_characters = string.punctuation + string.digits
+        for character in self.first_name + self.last_name:
+            if character in special_characters:
+                raise ValueError(f"Nom invalide {f"Nom invalide {self.full_name}."}")
+
 if __name__ == "__main__":
     from faker import Faker
     fake = Faker(locale="fr_FR")
     user = User(first_name=fake.first_name(),
                 last_name=fake.last_name(),
-                phone_number="13",
+                phone_number=fake.phone_number(),
                 address=fake.address(),)
-    user._check_phone_number()
+    user._check_names()
