9e546de89bdffc08ff5b9c20d3680840a2ac98f0
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2024-05-04 22:28:57 +0200

    learn typer: ask confirmation for use delete option

--- learn/typer/main_01.py
+++ learn/typer/main_01.py
@@ -7,5 +7,14 @@ def main(delete: bool = typer.Option(False, help="Supprime les fichiers trouvés
     print(delete)
     typer.echo(f"Recherche des fichiers avec l'extension {extension}.")
 
+    if delete:
+        confirm = typer.confirm("Souaitez vous supprimer les fichiers", abort=True)
+        # if not confirm:
+        #     typer.echo("Opération annulée.")
+        #     raise typer.Abort()
+    
+    print("Suppression des fichiers.")
+
+
 if __name__ == "__main__":
     typer.run(main)
