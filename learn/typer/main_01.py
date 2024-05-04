096f6e057b073ad453a8b7b27db76f1e1216e074
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2024-05-04 22:44:34 +0200

    learn typer: creat command search and delete for print/echo a message about file

--- learn/typer/main_01.py
+++ learn/typer/main_01.py
@@ -1,10 +1,12 @@
 import typer
 
+app = typer.Typer()
+
 def main(delete: bool = typer.Option(False, help="Supprime les fichiers trouvés"), extension: str = typer.Argument(..., help="Extension à chercher")):
     """
     Affiche les fichiers trouvés avec l'extension données.
     """
-    print(delete)
+    #print(delete)
     typer.echo(f"Recherche des fichiers avec l'extension {extension}.")
 
     if delete:
@@ -12,9 +14,16 @@ def main(delete: bool = typer.Option(False, help="Supprime les fichiers trouvés
         # if not confirm:
         #     typer.echo("Opération annulée.")
         #     raise typer.Abort()
-    
-    print("Suppression des fichiers.")
+        print("Suppression des fichiers.")
 
+@app.command("search")
+def search_py():
+    main(delete=False, extension="py")
+
+@app.command("delete")
+def delete_py():
+    main(delete=True, extension="py")
 
 if __name__ == "__main__":
-    typer.run(main)
+    #typer.run(main)
+    app()
