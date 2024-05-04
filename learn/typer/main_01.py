3fc144146e01add8ae8cfa26c02c53ee2cb9f16e
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2024-05-04 22:12:41 +0200

    learn typer: add a option (delete)

--- learn/typer/main_01.py
+++ learn/typer/main_01.py
@@ -1,10 +1,10 @@
 import typer
 
-def main(extension: str = typer.Argument(..., help="Extension à chercher")):
+def main(delete: bool = typer.Option(False, help="Supprime les fichiers trouvés"), extension: str = typer.Argument(..., help="Extension à chercher")):
     """
     Affiche les fichiers trouvés avec l'extension données.
     """
-
+    print(delete)
     typer.echo(f"Recherche des fichiers avec l'extension {extension}.")
 
 if __name__ == "__main__":
