ba45ad3a1ca1b089a8930897390413d0f83e7a15
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2024-05-05 16:38:20 +0200

    learn typer: implement the code for main function (can delete file and print the files found)

--- learn/typer/app/main_app.py
+++ learn/typer/app/main_app.py
@@ -1,5 +1,6 @@
 import typer
 from typing import Optional
+from pathlib import Path
 
 def main(extension: str, 
          directory: Optional[str] = typer.Argument(None, help="Dossier dans lequel chercher."),
@@ -8,7 +9,24 @@ def main(extension: str,
     """Affiche les fichiers trouvés avec l'extension donnée.
     """
 
-    print(extension, directory, delete)
+    if directory:
+        directory = Path(directory)
+    else:
+        directory = Path.cwd()
 
+    if not directory.exists():
+        typer.secho(f"Le dossier {directory} n'existe pas.", fg=typer.colors.RED)
+        raise typer.Exit()
+
+    files = directory.rglob(f"*.{extension}")
+    if delete:
+        typer.confirm("Voulez-vous vraiment supprimer tous les fichiers trouvés ?", abort=True)
+        for file in files:
+            file.unlink()
+            typer.secho(f"Fichier : {file} supprimé", fg=typer.colors.RED)
+    else:
+        for file in files:
+            typer.secho(f"Fichier : {file} trouvé", fg=typer.colors.BLUE)
+        
 if __name__ == "__main__":
     typer.run(main)
