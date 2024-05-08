abb793164667c6a19d70ce6c262bfed3cb7f6b37
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2024-05-08 18:07:25 +0200

    project 13: creat a method for add a movie

--- projects/project_13_cine_club/movie.py
+++ projects/project_13_cine_club/movie.py
@@ -1,30 +1,38 @@
 import os
 import json
+import logging
 
 CUR_DIR = os.path.dirname(__file__)
 DATA_File = os.path.join(CUR_DIR, "data", "movies.json")
+
 class Movie():
     def __init__(self, title = str):
-        self.title = title.title
+        self.title = title.title()
 
     def __str__(self):
         return self.title()
     
     def _get_movies(self):
         with open(DATA_File, "r") as file:
-         return json.load(file)
+            return json.load(file)
 
     def _write_movies(self, movies):
        with open(DATA_File, "w") as file:
-         json.dump(movies, file, indent=4)
+            json.dump(movies, file, indent=4)
+
+    def add_to_movies(self) -> bool:
+       list_movies = self._get_movies()
+       if self.title not in list_movies:
+           list_movies.append(self.title)
+           self._write_movies(list_movies)
+           return True
+       else:
+            logging.warning(f"Le film {self.title} est déjà dans la liste.")
+            return False
+          
 
 if __name__ == "__main__":
     m = Movie("harry potter")
-    print(m)
-    m._write_movies([
-    "le seigneur des anneaux",
-    "harry potter",
-    "transformers",
-    "jurassic park 2"])
-    print(m._get_movies())
+    m.add_to_movies()
+ 
  
