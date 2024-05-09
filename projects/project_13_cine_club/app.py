50f0665deeb665c9bf7d5de78a25bd28296ca0e8
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2024-05-09 17:39:28 +0200

    project 13: add movies in the widget list

--- projects/project_13_cine_club/app.py
+++ projects/project_13_cine_club/app.py
@@ -1,1 +1,36 @@
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtCore
+from movie import get_movies
+
+class App(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Ciné Club")
+        self.setup_ui()
+        self.resize(500, 800) 
+        self.populate_movies()
+
+    def setup_ui(self):
+        self.layout = QtWidgets.QVBoxLayout(self)
+
+        self.le_movieTitle = QtWidgets.QLineEdit()
+        self.btn_addMovie = QtWidgets.QPushButton("Ajouter un film")
+        self.lw_movies = QtWidgets.QListWidget()
+        self.btn_removeMovies = QtWidgets.QPushButton("Supprimer le(s) film(s)")
+
+        self.layout.addWidget(self.le_movieTitle)
+        self.layout.addWidget(self.btn_addMovie)
+        self.layout.addWidget(self.lw_movies)
+        self.layout.addWidget(self.btn_removeMovies)
+
+    def populate_movies(self):
+        movies = get_movies()
+
+        for movie in movies:
+            lw_item = QtWidgets.QListWidgetItem(movie.title)
+            lw_item.setData(QtCore.Qt.UserRole, movie)
+            self.lw_movies.addItem(lw_item)
+
+app = QtWidgets.QApplication([])
+win = App()
+win.show()
+app.exec_()
