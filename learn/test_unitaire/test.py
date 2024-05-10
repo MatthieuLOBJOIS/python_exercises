5bb87b35390515633285e2a05973bbd37c8da6ab
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2024-05-10 18:55:36 +0200

    learn: add string, bool and TypeError for None value test

--- learn/test_unitaire/test.py
+++ learn/test_unitaire/test.py
@@ -5,3 +5,16 @@ from main import add
 class TestCalculatrice(TestCase):
     def test_add_two_numbers(self):
         self.assertEqual(add(5, 10), 15)
+
+    def test_add_with_two_letters(self):
+        self.assertEqual(add("a", "b"), "ab")
+
+    def test_add_with_two_booleans(self):
+        self.assertEqual(add(True, False), 1)
+        self.assertEqual(add(True, True), 2)
+        self.assertEqual(add(False, False), 0)
+
+    def test_add_with_two_none(self):
+        with self.assertRaises(TypeError):
+            add(None, None)
+
