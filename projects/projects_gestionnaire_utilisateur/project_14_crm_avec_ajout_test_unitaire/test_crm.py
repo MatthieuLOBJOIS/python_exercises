3eb370bbdc48153baf52921038e792200b215dc4
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2024-05-13 20:17:23 +0200

    project 14: init test unit file

---
+++ projects/projects_gestionnaire_utilisateur/project_14_crm_avec_ajout_test_unitaire/test_crm.py
@@ -0,0 +1,20 @@
+def test_full_name():
+    assert False
+
+def test_db_instance():
+    assert False
+
+def test_check_phone_number():
+    assert False
+
+def test_check_names():
+    assert False
+
+def test_exists():
+    assert False
+    
+def test_delete():
+    assert False
+
+def test_save():
+    assert False
