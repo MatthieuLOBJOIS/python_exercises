1c252a109883659ad40d53b165442d74897729e7
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2024-05-13 20:56:03 +0200

    project 14: creat the fixtures

--- projects/projects_gestionnaire_utilisateur/project_14_crm_avec_ajout_test_unitaire/test_crm.py
+++ projects/projects_gestionnaire_utilisateur/project_14_crm_avec_ajout_test_unitaire/test_crm.py
@@ -1,3 +1,22 @@
+from crm import User
+import pytest
+from tinydb import TinyDB
+from tinydb.storages import MemoryStorage
+
+@pytest.fixture
+def setup_db():
+# Utilisation de la base de donnée en mémoire lors des tests (permet de ne pas créer de fichier supplémentaire)
+    User.DB = TinyDB(storage=MemoryStorage)
+
+@pytest.fixture
+def user(setup_db):
+    setup_db
+    u = User(first_name="Partick",
+             last_name="Martin",
+             address="1 rue du chemin, 75000 Paris",
+             phone_number="0123456789")
+# Sauvegarde un utilisateur en mémoire. 
+    u.save()
 def test_full_name():
     assert False
 
