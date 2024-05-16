64e0a9e7f15538940b15f193d3c59e353544c227
MatthieuLOBJOIS <lobjois.matthieu.pro@gmail.com>
2024-05-16 20:12:06 +0200

    project 15: insert data in a template

--- projects/project_15_django_premier_site_web/DocBlog/src/DocBlog/views.py
+++ projects/project_15_django_premier_site_web/DocBlog/src/DocBlog/views.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 
 def index(request):
-    return render(request, "index.html")
+    date = datetime.today()
+    return render(request, "index.html", context={"prenom": "Matthieu", "date": date})
